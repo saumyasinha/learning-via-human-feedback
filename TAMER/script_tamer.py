40669f00c76db466a4bef5553896dc14c93c57ac
saumyasinha <saumya.sinha@colorado.edu>
2020-04-07 21:10:13 -0600

    replace old script with one taking human input

--- TAMER/script_tamer.py
+++ TAMER/script_tamer.py
@@ -2,32 +2,63 @@ from TAMER.TAMER_Agent import TamerAgent, experience
 import gym
 import time
 import numpy as np
+import pygame
 
+interval = 60
+fps = 60
 
-# def getHumanSignal():
+def getHumanSignal(screen, lasttime):
+
+    pressed = pygame.key.get_pressed()
+    now = time.time()
+    # positive signal ((now-lasttime)>interval is to overcome the effect of human pressing the key for longer milliseconds
+    if pressed[pygame.K_w] and (now-lasttime)>interval:
+        screen.fill((0,255,0))
+        lasttime = now
+        return 1, lasttime
+    #negative signal
+    elif pressed[pygame.K_a] and (now-lasttime)>interval:
+        screen.fill((255,0,0))
+        lasttime = now
+        return -1, lasttime
+    else:
+        screen.fill((0, 0, 0))
+        return 0, lasttime
 
 
 ## Attempt to implement Algorithm1 of the Deep TAMER paper
-if __name__ == "__main__":
-    env = gym.make("MountainCar-v0")
+if __name__ == '__main__':
+    env = gym.make('MountainCar-v0')
 
     env.reset()
+    env.render()
 
     ## Initialize Tamer Agent
     agent = TamerAgent(env)
 
     ## hyperparameters
-    num_episodes = 20
-    buffer_update_interval = 50
+    num_episodes = 100
+    buffer_update_interval=75
 
     reward_list = []
     avg_reward_list = []
 
+
+    ## pygame initialization
+    pygame.init()
+    screen = pygame.display.set_mode((64, 48))
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    clock = pygame.time.Clock()
+
+
     for episode in range(num_episodes):
 
         done = False
+        is_exit = False
         tot_reward, reward = 0, 0
         state = env.reset()
+        lasttime = 0  # when was the last time human input is taken
 
         ## D (list of all experiences in form of (x,y) pairs)
         agent.total_experiences = []
@@ -38,10 +69,7 @@ if __name__ == "__main__":
         i = 0
         j = 0
         k = 0
-        while done != True:
-            # Render environment for last five episodes
-            # if i >= (num_episodes - 5):
-            #     env.render()
+        while done!=True and is_exit!=True:
 
             ## Choosing action via a greedy policy; action = argmax H(s,a)
             action = agent.getAction(state)
@@ -56,40 +84,71 @@ if __name__ == "__main__":
             state2, reward, done, info = env.step(action)
             tot_reward += reward
 
-
-            ## Proxy for receiving human signal/feedback (has to be replaced)
-            signal = np.random.choice([0, 1, 2, 3], 1, p=[0.8, 0.05, 0.05, 0.1])
-
-            # signal = getHumanSignal()
+            # receive human signal (if any)
+            signal,lasttime = getHumanSignal(screen, lasttime)
+            pygame.display.flip()
+            # print(signal)
 
             ## If human gives a feedback
-            if signal != 0:
+            if signal!=0:
 
                 feedback_time = time.time()
                 ## Update weights (assigning credit) of all x and build the set Dj for the current feedback
                 agent.updateWeightsforSignal(signal, feedback_time)
-                j = j + 1
+                j=j+1
 
                 ## mini-batch SGD update of the reward model(H)
                 agent.SGD_update("human")
-                k = k + 1
+                k = k+1
+
 
             ##Feedback replay buffer used in the paper: update the model at regular intervals
-            if (i % buffer_update_interval) == 0 and len(agent.total_experiences) > 0:
+            if (i%buffer_update_interval)==0 and len(agent.total_experiences)>0:
                 agent.SGD_update("fixed")
-                k = k + 1
+                k=k+1
+
 
             state = state2
 
+            i=i+1
 
-            i = i + 1
+            # process pygame event queue
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_exit = True
+                    break
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                    is_exit = True
+                    break
+
+            env.render()
+
+            # setting fps for pygame
+            clock.tick(fps)
 
         reward_list.append(tot_reward)
 
+        # obtain the average reward at every 100th episode
         if (episode + 1) % 100 == 0:
             avg_reward = np.mean(reward_list)
             avg_reward_list.append(avg_reward)
             reward_list = []
 
         if (episode + 1) % 100 == 0:
-            print("Episode {} Average Reward: {}".format(i + 1, avg_reward))
+            print('Episode {} Average Reward: {}'.format(episode + 1, avg_reward))
+
+
+    env.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
