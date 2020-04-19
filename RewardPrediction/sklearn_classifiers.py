e54adddc768be8f3b7f8be7ec52a443926cd7770
saumyasinha <saumya.sinha@colorado.edu>
2020-04-18 22:03:24 -0600

    add a function to predict output for a heldout data point

--- RewardPrediction/sklearn_classifiers.py
+++ RewardPrediction/sklearn_classifiers.py
@@ -2,11 +2,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.datasets import make_classification
 
+
+## num of AU probabilities extracted as an output of face classifier model
 n_features = 16
-classifiers = ["Linear SVM", "RBF SVM", "Random Forest"]
+classifiers = ["Linear SVM", "RBF SVM", "Random Forest", "AdaBoost"]
 
 
 def get_dataset():
@@ -24,7 +26,8 @@ def classfier_mapping():
     clf_mapping = {
         "Linear SVM" : SVC(kernel="linear", C=0.025),
         "RBF SVM" : SVC(gamma=2, C=1),
-        "Random Forest" : RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)
+        "Random Forest" : RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+        "AdaBoost" : AdaBoostClassifier()
     }
 
     return clf_mapping
@@ -36,7 +39,7 @@ def train(X, y, clf_name):
     :param X: input with 'n_features' features
     :param y: binary output
     :param clf_name: the name of the classifier used to train the model
-    :return: model predictions of input
+    :return: leaned classifier
     '''
 
     clf= classfier_mapping()[clf_name]
@@ -50,13 +53,25 @@ def train(X, y, clf_name):
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.15, random_state=1)
 
     clf.fit(X_train, y_train)
+
     score = clf.score(X_val, y_val)
 
     print("Validation Accuracy of "+clf_name+":"+str(score))
 
-    predictions = clf.predict(X)
+    return clf
 
-    return predictions
+
+def get_prediction(X_new, model):
+    '''
+
+    :param X_new: new input (from heldout set)
+    :param model: learned classifier
+    :return: model prediction of X_new
+    '''
+
+    prediction = model.predict(X_new)
+    return prediction
+
 
 
 ## "main" would not be needed since the functions from the module will be called directly from the final training script
@@ -64,7 +79,12 @@ if __name__ == "__main__":
 
     X,y = get_dataset()
 
-    reward_input_for_tamer = train(X,y,"Linear SVM")
+    model = train(X,y,"Linear SVM")
+
+    X_heldout = np.random.rand(1,n_features)
+    reward_input_for_tamer = get_prediction(X_heldout, model)
+
+
 
 
 
