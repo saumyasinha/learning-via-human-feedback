0989850d11e43e20712e9d21031f3ef50e490dbc
Michael Lauria <michael.david.lauria@gmail.com>
2020-04-20 15:37:51 -0600

    scale video to target

--- VideoCap/videocap.py
+++ VideoCap/videocap.py
@@ -9,6 +9,17 @@ import face_recognition
 import argparse
 
 
+def resize_frame(frame, frame_shape, target_shape):
+    frame_remainder = tuple(t1 % t2 for t1, t2 in zip(frame_shape, target_shape))
+    crop_width = frame_remainder[0] // 2
+    crop_height = frame_remainder[1] // 2
+    height, width = frame_shape
+    target_height, target_width = target_shape
+    frame = frame[crop_height : height - crop_height, crop_width : width - crop_width]
+    frame = cv2.resize(frame, (target_width, target_height))
+    return frame
+
+
 def capture_webcam(output_dir):
     video_capture = cv2.VideoCapture(0)
 
@@ -16,13 +27,15 @@ def capture_webcam(output_dir):
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 
     # Need to scale to this height and width to match Affectiva input
-    target_width = 240
-    target_height = 320
+    target_width = 320
+    target_height = 240
+    target_shape = (target_height, target_width)
 
     frame_width = int(video_capture.get(3))
     frame_height = int(video_capture.get(4))
+    frame_shape = (frame_height, frame_width)
 
-    print(f"frame (width, height): ({frame_width}, {frame_height})")
+    print(f"frame (height, width): {frame_shape}")
 
     video_fps = 14
 
@@ -35,7 +48,9 @@ def capture_webcam(output_dir):
         while True:
             # Grab a single frame of video
             ret, frame = video_capture.read()
-            frame = cv2.resize(frame, (target_width, target_height))
+            frame = resize_frame(
+                frame, frame_shape=frame_shape, target_shape=target_shape
+            )
 
             # Display the resulting image
             cv2.imshow("Video", frame)
