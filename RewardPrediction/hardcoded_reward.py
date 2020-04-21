93141ce0377e7368c05216a47a10ed82a2b917d9
saumyasinha <saumya.sinha@colorado.edu>
2020-04-21 16:46:57 -0600

    clean up and comment in the reward prediction module

--- RewardPrediction/hardcoded_reward.py
+++ RewardPrediction/hardcoded_reward.py
@@ -1,9 +1,15 @@
 ## Followed this blog :
 ## https://imotions.com/blog/facial-action-coding-system/#emotions-action-units
 
-
+## List of AUs from the facial classifier model
+# AU_list = ['Unilateral_RAU14', 'AU17', 'AU02', 'AU25', 'AU20', 'Unilateral_RAU12', 'AU05', 'Unilateral_LAU14', 'AU18', 'AU14', 'AU26', 'AU06', 'Unilateral_LAU12', 'AU04', 'Neutral']
 
 def au_to_reward_mapping(prob_mapping, threshold):
+    '''
+    :param prob_mapping: dictionary mapping the AU names to their probabilities (output from facial classifier model)
+    :param threshold: threshold to hardcode the reward output
+    :return: reward mapped to the input set of probabilities
+    '''
 
     ## Happiness / Joy
     if (prob_mapping['AU06'] + prob_mapping['Unilateral_RAU12'] + prob_mapping['Unilateral_LAU12']) > threshold:
