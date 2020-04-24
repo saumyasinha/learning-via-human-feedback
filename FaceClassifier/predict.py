c34c1b94bf8d41f112726d7ec8e98a029f6425e7
vikasnataraja <viha4393@colorado.edu>
2020-04-24 14:40:32 -0600

    modified face classfier model loading

--- FaceClassifier/predict.py
+++ FaceClassifier/predict.py
@@ -19,13 +19,14 @@ def preprocess(img, resize_dims):
   return img
 
 
-def prediction(img_path, model_path, classes):
+def prediction(img_path, model=None, model_path=None, classes=None):
   """
   Uses a trained Keras model to make predict the probability distributin of facial action units.
   The `prediction` fuction returns a dictionary mapping class names i.e AUs to their probability scores. 
 
   Args:
   - img_path: str, path to image
+  - model: keras.models.Model, use a pre-loaded model to increase speed
   - model_path: str, path to the trained model
   - classes: list of classes e.g. ['AU04','AU05']
 
@@ -33,14 +34,16 @@ def prediction(img_path, model_path, classes):
   A dictionary mapping the class labels to the precicted probability scores
 
   """
+  if model is None:
+    # load the model if not loaded
+    model = load_model(model_path)
   
-  # load the model and predict
-  model = load_model(model_path)
   # read the image and preprocess it
   img = cv2.imread(img_path)
   img = preprocess(img, resize_dims=(model.input_shape[1],model.input_shape[2]))
   predictions = model.predict(img).flatten()
-  # match the predictions to the classes
-  print('Predicted class probabilities:\n {}'.format(dict(zip(classes,list(predictions)))))
+  if classes is None:
+    classes = list(np.arange(0,len(list(predictions))))
+  
   return dict(zip(classes,list(predictions)))
 
