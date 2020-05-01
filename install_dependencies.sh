c85e73687ad2965680c41139a30b3cf074d0f34c
vikasnataraja <viha4393@colorado.edu>
2020-04-30 19:52:50 -0600

    updated dependencies to accommodate pygame-dev

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,17 @@ echo "Installing Python 3.8"
 sudo apt-get install python3.8
 
 # additional dependencies, mostly for pygame, asyncio and pandas
-sudo apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev
+sudo apt-get install python-dev libsdl-ttf2.0-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev
+
+# sdl-config
+sudo apt install libsdl2-dev libsdl2-2.0-0 -y;
+
+# sdl-image
+sudo apt install libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 -y;
+
+#install sdl mixer
+sudo apt install libmikmod-dev libfishsound1-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 -y;
+
 
 echo "Installing packages and libraries from requirements.txt"
 python3.8 -m pip install -r requirements.txt
