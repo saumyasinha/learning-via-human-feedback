b1cce996d947e74fae80dd09c6e7fb7830f71b3d
vikasnataraja <viha4393@colorado.edu>
2020-05-19 16:03:05 -0600

    move functions to utils and add annealing

--- FaceClassifier/utils/utils.py
+++ FaceClassifier/utils/utils.py
@@ -4,7 +4,61 @@ from imutils import face_utils
 import dlib
 import cv2
 from keras.utils import to_categorical
+from keras.layers import Layer
+from keras.callbacks import Callback
+import keras.backend as K
+import tensorflow as tf
 import os
+
+
+def BatchNorm():
+    return BatchNormalization(momentum=0.95, epsilon=1e-5)
+
+
+class Resize(Layer):
+    """ Custom Keras layer that resizes to a new size using interpolation.
+    Bypasses the use of Keras Lambda layer
+    Args:
+      - new_size: tuple, new size to which layer needs to be resized to. Must be (height, width)
+      - method: str, method of interpolation to be used. If None, defaults to bilinear.
+               Choose amongst 'bilinear', 'nearest', 'lanczos3', 'lanczos5', 'area', 'gaussian', 'mitchellcubic'
+    Returns:
+      - keras.layers.Layer of size [?, new_size[0], new_size[1], depth]
+    """
+
+    def __init__(self, new_size, method="bilinear", **kwargs):
+        self.new_size = new_size
+        self.method = method
+        super(Resize, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(Resize, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        resized_height, resized_width = self.new_size
+        return tf.image.resize(
+            images=inputs,
+            size=[resized_height, resized_width],
+            method=self.method,
+            # align_corners=True,
+        )
+
+    def compute_output_shape(self, input_shape):
+        return tuple([None, self.new_size[0], self.new_size[1], input_shape[3]])
+
+    def get_config(self):
+        config = super(Resize, self).get_config()
+        config["new_size"] = self.new_size
+        return config
+
+class AnnealingCallback(Callback):
+    def __init__(self, weight):
+        self.weight = weight
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch > klstart :
+            new_weight = min(K.get_value(self.weight) + (1./ annealtime), 1.)
+            K.set_value(self.weight, new_weight)
+        print ("Current KL Weight is " + str(K.get_value(self.weight)))
 
 
 def get_landmark_points(img, detector, predictor):
