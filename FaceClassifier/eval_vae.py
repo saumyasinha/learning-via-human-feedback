37c7961292089aa15f550da8d8647661026c329b
Michael Lauria <michael.david.lauria@gmail.com>
2020-05-18 22:34:01 -0600

    fix eval for vae

--- FaceClassifier/eval_vae.py
+++ FaceClassifier/eval_vae.py
@@ -8,27 +8,23 @@ from model import DECODER, ENCODER
 
 from predict import prediction_on_frame
 
-model_name = "vae_overnight"
-weights = f"weights/{model_name}.h5"
+model_name = "vae_may18_vae"
+weights = f"vae_weights/{model_name}.h5"
 
 ENCODER.load_weights(weights, by_name=True)
 DECODER.load_weights(weights, by_name=True)
-DECODER.summary()
-
-height = 240
-width = 320
 
 # z_sample = np.random.normal(loc=0, scale=1, size=(1, 20))
 # figure = DECODER.predict(z_sample)[0]
 folder = "imgs/0bebd5c1-ea42-4c80-a311-b5f9622accdb"
 preds = []
 for filename in os.listdir(folder):
-    frame = cv2.imread(os.path.join(folder, filename))
+    frame = cv2.imread(os.path.join(folder, filename)) / 255
     encoded = ENCODER.predict(np.expand_dims(frame, axis=0))
     decoded = DECODER.predict(encoded[2])
     break  # just do one for now
 
-plt.imshow(frame)
+plt.imshow(np.rint(frame * 255).astype(int))
 plt.show()
-plt.imshow(decoded[0])
+plt.imshow(np.rint(decoded[0] * 255).astype(int))
 plt.show()
