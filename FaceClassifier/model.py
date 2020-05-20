c5a7c5cf0b37c3b7e8931a39a8f5befcb91626fb
Michael Lauria <michael.david.lauria@gmail.com>
2020-05-19 20:57:28 -0600

    load vae weights to speed training

--- FaceClassifier/model.py
+++ FaceClassifier/model.py
@@ -243,6 +243,7 @@ def vae_network(num_classes, final_activation_fn="sigmoid"):
 ENCODER, DECONV_SHAPE = get_encoder()
 DECODER = get_decoder(shape=DECONV_SHAPE)
 VAE, VAE_LOSS = get_vae(ENCODER, DECODER)
+VAE.load_weights("vae_weights/vae_may19_vae.h5")
 
 
 def landmark_network(input_shape, num_classes, final_activation_fn="sigmoid"):
